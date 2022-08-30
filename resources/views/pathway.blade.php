37d7858aebb56c9229a789ea704de713c9242429
gul-amd <100712631+gul-amd@users.noreply.github.com>
2022-08-30 21:21:34 +0200

    build: pathway

---
+++ resources/views/pathway.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('statics/img/apple-icon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('statics/img/favicon.png') }}">
+  <title>
+    Argon Dashboard 2 by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="{{ asset('statics/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ asset('statics/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="{{ asset('statics/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('statics/css/argon-dashboard.css?v=2.0.4') }}" rel="stylesheet" />
+</head>
+
+<body class="g-sidenav-show bg-gray-100 ">
+
+    <div class="container-fluid py-12">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="row">
+
+                    <div class="col-lg-6">
+                        <div class="row">
+
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-header mx-4 p-3 text-center">
+                                        <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
+                                            <i class="fas fa-dolly-flatbed opacity-10"></i>
+                                        </div>
+                                    </div>
+                                    <div class="card-body pt-0 p-3 text-center">
+                                        <h6 class="text-center mb-0">Warehouse</h6>
+                                        <span class="text-xs">Fishing Gear & Packaging</span>
+                                        <hr class="horizontal dark my-1">
+                                        <h5 class="mb-0">Armazens</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-4 mt-md-0 mt-4">
+                                <div class="card">
+                                    <div class="card-header mx-4 p-3 text-center">
+                                        <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
+                                            <i class="fas fa-fish opacity-10"></i>
+                                        </div>
+                                    </div>
+                                    <div class="card-body pt-0 p-3 text-center">
+                                        <h6 class="text-center mb-0">Cold Stores</h6>
+                                        <span class="text-xs">H&G / Value added / Baits</span>
+                                        <hr class="horizontal dark my-1">
+                                        <h5 class="mb-0">Frigorificos</h5>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 mt-md-0 mt-4">
+                                <div class="card">
+                                    <div class="card-header mx-4 p-3 text-center">
+                                        <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
+                                            <i class="fas fa-cogs opacity-10"></i>
+                                        </div>
+                                    </div>
+                                    <div class="card-body pt-0 p-3 text-center">
+                                        <h6 class="text-center mb-0">Production</h6>
+                                        <span class="text-xs">Daily Processing</span>
+                                        <hr class="horizontal dark my-1">
+                                        <h5 class="mb-0">Producao</h5>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  
+</body>
+
+</html>
