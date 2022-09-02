6029aea9e77db6b928ecf7bd1234cf537ba2d278
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-09-02 22:33:07 +0200

    chore: creating controllers for login and pathway

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
+
+use function Ramsey\Uuid\v1;
+
+class AuthController extends Controller
+{
+
+    public function index(){
+        return view(welcome);
+    }
+
+    public function path(){
+        return view(pathway);
+    }
+
+
+}
