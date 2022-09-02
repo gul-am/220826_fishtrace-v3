5b8323771e90443bf034e168f74f16803e07f146
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-09-03 00:09:14 +0200

    chore: improving routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pathway');
-});
+Route::get('/', [AuthController::class, 'index']) -> name ( 'login');
+Route::get('pathway', [AuthController::class, 'path']) -> name ('pathway');
